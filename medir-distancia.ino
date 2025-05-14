c3ebbda72de3b2773245a7523c42d470fbf87595
francisca <125706199+YooPuiPui@users.noreply.github.com>
2025-05-14 13:52:26 -0400

    update

--- medir-distancia.ino
+++ medir-distancia.ino
@@ -2,56 +2,53 @@ const int pin1 = 2;
 const int pin2 = 3;
 
 volatile long Pos = 0;
-long lasPos = 0;
+long lastPos = 0;
 
-const float pi = 2.1416;
+const float pi = 3.1416;
 const float R = 3.25;
-const N = 4860;
+const int N = 4860;
 
 float distance = 0;
 
 void setup() {
-  PinMode(pin1, INPUT_PULLUP);
-  PinMode(pin2, INPUT_PULLUP);
+  pinMode(pin1, INPUT_PULLUP);
+  pinMode(pin2, INPUT_PULLUP);
   Serial.begin(9600);
-  Serial.println("MEASURIN WHEEL");
+  Serial.println("MEASURING WHEEL");
 
-  // Configura una interrupcion en pin 1 para ejecutarse en cualquier cambio de estado
   attachInterrupt(digitalPinToInterrupt(pin1), encoderISR, CHANGE);
 
   Pos = 0;
   lastPos = 0;
-  Serial.println("Posicion inicial: 0 cm");
-  
-  // put your setup code here, to run once:
-
+  Serial.println("Posición inicial: 0 cm");
 }
 
 void loop() {
-  distance = ((2*pi*R)/N)*Pos;
+  distance = ((2 * pi * R) / N) * Pos;
 
-  if(Pos != lastPos){
+  if (Pos != lastPos) {
     Serial.print("Pos: ");
     Serial.print(Pos);
     Serial.print(" | Distance: ");
     Serial.print(distance, 2);
-    Serial.print(" cm");
+    Serial.println(" cm");
     lastPos = Pos;
   }
 }
 
-void encoderISR(){
+void encoderISR() {
   static unsigned long lastTime = 0;
   unsigned long now = micros();
 
-  if(now - lasTime > 1000){
+  if (now - lastTime > 1000) {
     bool A = digitalRead(pin1);
     bool B = digitalRead(pin2);
 
-    if(A == B) Pos++;
-    else Pos--;
+    if (A == B)
+      Pos++;
+    else
+      Pos--;
 
     lastTime = now;
   }
-  
 }
